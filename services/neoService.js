81d68f5ec0b78ebb037412c5057426f6138ffeff
sidspencer <sidspencer@gmail.com>
2019-11-01 21:48:14 -0500

    Fix to be less-than-or-equal

--- services/neoService.js
+++ services/neoService.js
@@ -4,7 +4,7 @@ class NeoService {
 
         for (let dateKey in neosByDate) {
             neosByDate[dateKey].forEach((neo) => {
-                if (neo.close_approach_data[0].miss_distance.miles < distance) {
+                if (neo.close_approach_data[0].miss_distance.miles <= distance) {
                     if (closeNeos.indexOf(neo.name) === -1) {
                         closeNeos.push(neo.name);
                     }
